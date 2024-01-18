c1d35aeb56f86c4718929848d9bef82dfcbcdcd9
Nico J <niicoo995@gmail.com>
2024-01-18 12:39:50 -0300

    añadido farmacia

---
+++ assets/js/farmacia.js
@@ -0,0 +1,50 @@
+import { productos } from "./api.js";
+const $containerId = document.getElementById("container1");
+const $inputText = document.getElementById(`inputText`);
+
+function cardMaker(productos, $containerId) {
+  if ($containerId) {
+    $containerId.innerHTML = "";
+    const cardHtml = productos.reduce((acumulador, producto) => {
+      const disponiblesTexto =
+        producto.disponibles < 5
+          ? `<p>Disponibles: ${producto.disponibles} ¡Pocas unidades!</p>`
+          : `<p>Disponibles: ${producto.disponibles}</p>`;
+      const estadoDisponibilidad =
+        producto.disponibles === 0
+          ? `<p>Producto agotado</p>`
+          : disponiblesTexto;
+
+      return `${acumulador}
+            <article class='product_card'>
+            <img src=${producto.imagen} alt="imagen">
+            <h1>${producto.producto}</h1>
+            <p class='price'>Precio: $${producto.precio}</p>
+            <p>${producto.descripcion}</p>
+            ${estadoDisponibilidad}
+            <a href="./details.html?id=${producto._id}">  
+           <button type="button" class='detailsBtn'>DETAILS</button>
+           <button id="${producto._id}"> AÑADIR AL CARRITO   </button>
+           </a>
+            </article>`;
+    }, "");
+    $containerId.innerHTML = cardHtml;
+  }
+}
+
+export const productosFarmacia = productos.filter(
+  (producto) => producto.categoria === "farmacia"
+);
+
+cardMaker(productosFarmacia, $containerId);
+
+if ($inputText) {
+  $inputText.addEventListener("input", () => {
+    const searchTerm = $inputText.value.toLowerCase();
+    const productosFiltrados = productosFarmacia.filter((producto) =>
+      producto.producto.toLowerCase().includes(searchTerm)
+    );
+
+    cardMaker(productosFiltrados, $containerId);
+  });
+}
